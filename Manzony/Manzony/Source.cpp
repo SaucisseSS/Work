1074345b1d53b031f4455ed8f2202236e6468e56
SaucisseSS <58030235+SaucisseSS@users.noreply.github.com>
2020-03-04 12:45:41 +0300

    Hellow GitHub

---
+++ Manzony/Manzony/Source.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+
+//using namespace std;
+using namespace cv;
+
+int main() {
+
+	//string nameOfVideo = "video.avi";
+
+	VideoCapture videoCapture;
+	videoCapture.open("video.avi");
+
+	
+
+	return 0;
+}
